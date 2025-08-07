afd5f1d2a5ddd7cbfacf7ac8c627f1d756ce89ef
wabemusic <jvwatanabe@gmail.com>
2025-08-06 21:29:15 -0300

    feat: login page, index.jsx

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,6 +1,35 @@
-import styles from "./login.module.css";
 
-export function Login(){
-    return <div className={styles.login}><h1>Login</h1>
-        </div>
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import api from "../../services/api";
+import Input from "../../components/Input";
+import Button from "../../components/Button";
+
+export default function Login() {
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    const navigate = useNavigate()
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+        try {
+            const {data} = await api.post('/login', {email, senha});
+            localStorage.setItem('token', data.token);
+            navigate('/dashboard')
+
+        } catch {
+            alert('Usuário ou senha inválidos');
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <Input label="E-Mail" type="email" value={email} onChange={e => setEmail(e.target.value)}/>
+            <Input label="senha" type="password" value={senha} onChange={e => setSenha(e.target.value)} />
+            <Button type="submit">Entrar</Button>
+            <Link to ="/forto-password">Esqueci minha senha</Link>
+        </form>
+    )
 }
+
+
